c65c4bce732588196a6d0a340bca699dd454bc6d
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2025-10-23 18:55:38 +0300

    Створення головної сторінки адміна + додавання стилів

---
+++ admin/admin_dashboard.php
@@ -0,0 +1,98 @@
+<?php
+@include '../../BookStore_BackEnd/config/Database.php';
+session_start();
+$admin_id = $_SESSION['admin_id'];
+if(!isset($admin_id)){
+   header('location:login.php');
+};
+?>
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Адмін-панель</title>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/admin_style.css">
+</head>
+<body>
+<?php include 'includes/admin_header.php'; ?>
+<section class="dashboard">
+   <h1 class="title">ПАНЕЛЬ АДМІНІСТРАТОРА</h1>
+   <div class="box-container">
+      <div class="box">
+         <?php
+            $total_pendings = 0;
+            $select_pendings = mysqli_query($conn, "SELECT * FROM orders WHERE payment_status = 'pending'") or die('Помилка запиту');
+            while($fetch_pendings = mysqli_fetch_assoc($select_pendings)){
+               $total_pendings += $fetch_pendings['total_price'];
+            };
+         ?>
+         <h3>₴<?php echo $total_pendings; ?>/-</h3>
+         <p>Загальна сума очікуваних платежів</p>
+      </div>
+      <div class="box">
+         <?php
+            $total_completes = 0;
+            $select_completes = mysqli_query($conn, "SELECT * FROM orders WHERE payment_status = 'completed'") or die('Помилка запиту');
+            while($fetch_completes = mysqli_fetch_assoc($select_completes)){
+               $total_completes += $fetch_completes['total_price'];
+            };
+         ?>
+         <h3>₴<?php echo $total_completes; ?>/-</h3>
+         <p>Завершені платежі</p>
+      </div>
+      <div class="box">
+         <?php
+            $select_orders = mysqli_query($conn, "SELECT * FROM orders") or die('Помилка запиту');
+            $number_of_orders = mysqli_num_rows($select_orders);
+         ?>
+         <h3><?php echo $number_of_orders; ?></h3>
+         <p>Кількість замовлень</p>
+      </div>
+      <div class="box">
+         <?php
+            $select_products = mysqli_query($conn, "SELECT * FROM products") or die('Помилка запиту');
+            $number_of_products = mysqli_num_rows($select_products);
+         ?>
+         <h3><?php echo $number_of_products; ?></h3>
+         <p>Додані товари</p>
+      </div>
+      <div class="box">
+         <?php
+            $select_users = mysqli_query($conn, "SELECT * FROM users WHERE user_type = 'user'") or die('Помилка запиту');
+            $number_of_users = mysqli_num_rows($select_users);
+         ?>
+         <h3><?php echo $number_of_users; ?></h3>
+         <p>Користувачі</p>
+      </div>
+      <div class="box">
+         <?php
+            $select_admin = mysqli_query($conn, "SELECT * FROM users WHERE user_type = 'admin'") or die('Помилка запиту');
+            $number_of_admin = mysqli_num_rows($select_admin);
+         ?>
+         <h3><?php echo $number_of_admin; ?></h3>
+         <p>Адміністратори</p>
+      </div>
+      <div class="box">
+         <?php
+            $select_account = mysqli_query($conn, "SELECT * FROM users") or die('Помилка запиту');
+            $number_of_account = mysqli_num_rows($select_account);
+         ?>
+         <h3><?php echo $number_of_account; ?></h3>
+         <p>Усього акаунтів</p>
+      </div>
+      <div class="box">
+         <?php
+            $select_messages = mysqli_query($conn, "SELECT * FROM message") or die('Помилка запиту');
+            $number_of_messages = mysqli_num_rows($select_messages);
+         ?>
+         <h3><?php echo $number_of_messages; ?></h3>
+         <p>Нові повідомлення</p>
+      </div>
+   </div>
+</section>
+<script src="../assets/js/admin_script.js"></script>
+</body>
+</html>
