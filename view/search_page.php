4258ca4ed2c6e0101a67c870d21aec223643ee76
anesqqta <anastasiya.semeniuk@kpk.lp-com.ua>
2025-10-26 22:43:44 +0200

    Корекція сторінки пошуку

--- view/search_page.php
+++ view/search_page.php
@@ -1,49 +1,44 @@
 <?php
-@include '../../BookStore_BackEnd/config/Database.php';
+require_once '../../BookStore_BackEnd/controllers/SearchController.php';
+require_once '../../BookStore_BackEnd/controllers/CartController.php';
+require_once '../../BookStore_BackEnd/controllers/WishlistController.php';
+
 session_start();
-$user_id = $_SESSION['user_id'];
-if(!isset($user_id)){
-   header('location:login.php');
+
+$user_id = $_SESSION['user_id'] ?? null;
+if (!$user_id) {
+    header('location:login.php');
+    exit;
 }
-if(isset($_POST['add_to_wishlist'])){
-   $message = [];
-   $product_id = $_POST['product_id'];
-   $product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
-   $product_price = $_POST['product_price'];
-   $product_image = $_POST['product_image'];
-   $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM wishlist WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-   if(mysqli_num_rows($check_wishlist_numbers) > 0){
-       $message[] = 'Товар вже додано до списку бажаного';
-   } else {
-       mysqli_query($conn, "INSERT INTO wishlist(user_id, book_id, name, price, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
-       $message[] = 'Товар додано до списку бажаного';
-   }
+
+$searchController = new SearchController();
+$cartController = new CartController();
+$wishlistController = new WishlistController();
+$message = [];
+
+// Додавання до списку бажаного
+if (isset($_POST['add_to_wishlist'])) {
+    $response = $wishlistController->addToWishlist($user_id, $_POST);
+    $message[] = $response;
 }
-if(isset($_POST['add_to_cart'])){
-   $message = [];
-   $product_id = $_POST['product_id'];
-   $product_name = mysqli_real_escape_string($conn, $_POST['product_name']);
-   $product_price = $_POST['product_price'];
-   $product_image = $_POST['product_image'];
-   $product_quantity = $_POST['product_quantity'];
-   $check_cart_numbers = mysqli_query($conn, "SELECT * FROM cart WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-   if(mysqli_num_rows($check_cart_numbers) > 0){
-       $message[] = 'Товар вже додано до кошика';
-   }else{
-       $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM wishlist WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-       if(mysqli_num_rows($check_wishlist_numbers) > 0){
-           mysqli_query($conn, "DELETE FROM wishlist WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
-       }
-       mysqli_query($conn, "INSERT INTO cart(user_id, book_id, name, price, quantity, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-       $message[] = 'Товар додано до кошика';
-   }
+
+// Додавання до кошика
+if (isset($_POST['add_to_cart'])) {
+    $response = $cartController->addToCart($user_id, $_POST);
+    $message[] = $response;
+}
+
+// Пошук товарів
+$results = [];
+if (isset($_POST['search_btn'])) {
+    $search_box = $_POST['search_box'];
+    $results = $searchController->searchProducts($search_box);
 }
 ?>
 <!DOCTYPE html>
 <html lang="uk">
 <head>
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Сторінка пошуку</title>
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -51,49 +46,49 @@ if(isset($_POST['add_to_cart'])){
 </head>
 <body>
 <?php include 'includes/header.php'; ?>
+
 <section class="heading">
-    <h3>Сторінка пошуку</h3>
-    <p> <a href="home.php">головна</a> / пошук </p>
+    <h3>СТОРІНКА ПОШУКУ</h3>
+    <p><a href="home.php">головна</a> / пошук</p>
 </section>
+
 <section class="search-form">
     <form action="" method="POST">
-        <input type="text" class="box" placeholder="пошук товарів..." name="search_box">
-        <input type="submit" class="option-btn" value="пошук" name="search_btn">
+        <input type="text" class="box" placeholder="Пошук товарів..." name="search_box" required>
+        <input type="submit" class="option-btn" value="Пошук" name="search_btn">
     </form>
 </section>
+
 <section class="products" style="padding-top: 0;">
    <div class="box-container">
-<?php
-        if(isset($_POST['search_btn'])){
-         $search_box = mysqli_real_escape_string($conn, $_POST['search_box']);
-         $select_products = mysqli_query($conn, "SELECT * FROM products WHERE name LIKE '%{$search_box}%'") or die('Запит не вдався');
-         if(mysqli_num_rows($select_products) > 0){
-            while($fetch_products = mysqli_fetch_assoc($select_products)){
-      ?>
-      <form action="" method="POST" class="box">
-         <a href="view_page.php?book_id=<?php echo $fetch_products['id']; ?>" class="fas fa-eye"></a>
-         <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
-         <img src="../uploaded_img/<?php echo $fetch_products['image']; ?>" alt="" class="image">
-         <div class="name"><?php echo $fetch_products['name']; ?></div>
-         <input type="number" name="product_quantity" value="1" min="0" class="qty">
-         <input type="hidden" name="product_id" value="<?php echo $fetch_products['id']; ?>">
-         <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-         <input type="hidden" name="product_price" value="<?php echo $fetch_products['price']; ?>">
-         <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
-         <input type="submit" value="додати в бажаний список" name="add_to_wishlist" class="option-btn">
-         <input type="submit" value="додати в кошик" name="add_to_cart" class="btn">
-      </form>
-      <?php
-         }
-            }else{
-                echo '<p class="empty">Не знайдено результатів!</p>';
-            }
-        }else{
-            echo '<p class="empty">Пошукайте щось!</p>';
-        }
-      ?>
+   <?php
+   if (isset($_POST['search_btn'])) {
+       if ($results->num_rows > 0) {
+           while ($product = $results->fetch_assoc()) {
+   ?>
+   <form action="" method="POST" class="box">
+       <a href="view_book.php?book_id=<?= $product['id']; ?>" class="fas fa-eye"></a>
+       <div class="price">₴<?= $product['price']; ?></div>
+       <img src="../uploaded_img/<?= $product['image']; ?>" alt="<?= htmlspecialchars($product['name']); ?>" class="image">
+       <div class="name"><?= htmlspecialchars($product['name']); ?></div>
+       <input type="number" name="product_quantity" value="1" min="1" class="qty">
+       <input type="hidden" name="product_id" value="<?= $product['id']; ?>">
+       <input type="hidden" name="product_name" value="<?= htmlspecialchars($product['name']); ?>">
+       <input type="hidden" name="product_price" value="<?= $product['price']; ?>">
+       <input type="hidden" name="product_image" value="<?= $product['image']; ?>">
+       <input type="submit" value="Додати до списку бажаного" name="add_to_wishlist" class="option-btn">
+       <input type="submit" value="Додати до кошика" name="add_to_cart" class="btn">
+   </form>
+   <?php
+           }
+       } else {
+           echo '<p class="empty">Нічого не знайдено!</p>';
+       }
+   }
+   ?>
    </div>
 </section>
+
 <?php include 'includes/footer.php'; ?>
 <script src="../assets/js/script.js"></script>
 </body>
